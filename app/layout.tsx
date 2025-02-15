833ffeaa39865cc07952cbc843ac00947803c9ef
latic1 <laticlatif2@gmail.com>
2025-02-15 16:43:51 +0000

    fix1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/nav-bar";
 import { Footer } from "@/components/Footer";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,21 +14,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Blue Bow Maritime Agency",
   description: "We specialize in offering a comprehensive range of services that include shipping, stevedoring, P&I insurance, and marine operations",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <Head>
+        {/* Global Meta Tags */}
+        <meta name="description" content="Blue Bow Maritime Agency provides comprehensive maritime services." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Blue Bow Maritime Agency" />
+        <meta property="og:description" content="Comprehensive maritime services including shipping, stevedoring, and marine operations." />
+        <meta property="og:image" content="/img/logo.jpeg" />
+        <meta property="og:url" content="www.bluebowmaritimelib.com" />
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
         <Footer />
